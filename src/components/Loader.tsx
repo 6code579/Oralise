70447f8894fd3bc24f65c2d9933979ba6cc0601c
6code579 <codefranc6co@hotmail.com>
2025-07-16 17:33:34 +0100

    some modifications

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,14 +2,13 @@
 const Loader = () => {
 
   return (
-  <div className="flex justify-center items-center min-h-screen 
-                  bg-gradient-to-tr from-black main-color">
+  <div className="flex justify-center items-center min-h-screen bg-white dark:bg-black ">
     <div className="relative w-[190px] h-[190px] flex justify-center items-center">
       <div className="absolute w-[190px] h-[190px] rounded-full border-b-[8px] border-b-pink-400 border-transparent border-solid animate-rotate1"></div>
       <div className="absolute w-[190px] h-[190px] rounded-full border-b-[8px] border-b-pink-600 border-transparent border-solid animate-rotate2"></div>
       <div className="absolute w-[190px] h-[190px] rounded-full border-b-[8px] border-b-cyan-400 border-transparent border-solid animate-rotate3"></div>
       <div className="absolute w-[190px] h-[190px] rounded-full border-b-[8px] border-b-yellow-400 border-transparent border-solid animate-rotate4"></div>
-      <div className="text-white absolute text-2xl font-bold select-none">Oralise</div>
+      <div className="text-black dark:text-white absolute text-2xl font-bold select-none">Oralise</div>
     </div>
   </div>
 );
