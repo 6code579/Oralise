a9c9c1fbf15de6afe4b080d74fdee20415a17617
6code579 <codefranc6co@hotmail.com>
2025-07-16 17:42:07 +0100

    Oralise color2

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -8,7 +8,7 @@ const Loader = () => {
       <div className="absolute w-[190px] h-[190px] rounded-full border-b-[8px] border-b-pink-600 border-transparent border-solid animate-rotate2"></div>
       <div className="absolute w-[190px] h-[190px] rounded-full border-b-[8px] border-b-cyan-400 border-transparent border-solid animate-rotate3"></div>
       <div className="absolute w-[190px] h-[190px] rounded-full border-b-[8px] border-b-yellow-400 border-transparent border-solid animate-rotate4"></div>
-      <div className="text-black dark:maiin-color absolute text-2xl font-bold select-none">Oralise</div>
+      <div className="text-black dark:main-color absolute text-2xl font-bold select-none">Oralise</div>
     </div>
   </div>
 );
